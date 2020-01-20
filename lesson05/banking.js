3f422ead04a8e7f7b11efca66061d4ec3bfa5338
ahendryx <aubreyhendryx@gmail.com>
2020-01-19 16:52:07 -0800

    (Adding Lesson 5)

--- lesson05/banking.js
+++ lesson05/banking.js
@@ -1,26 +1,30 @@
 function userOption() {
+  
 let input = prompt('What would you like to do?');
 
-/*
-while (input !== 'q') {
-input++;
-}
-*/
+  while (input !== 'q') {
+      if (input === 'w') {
+          prompt('How much would you like to withdraw?');
+          break;
+          }
+          else if (input === 'd') {
+          prompt('How much would you like to deposit?');
+          break;
+          }
+          else if (input === 'b') {
+          alert('Your account balance is $100.');
+          break;
+          }
+          else {
+          alert('Invalid entry. Please try again.');
+          break;
+          }
+   }
+   while (input === 'q') {
+      alert('Your transaction is complete.');
+      break;
+   }
 
-  if (input === 'w') {
-    prompt('How much would you like to withdraw?');
-    }
-    else if (input === 'd') {
-    prompt('How much would you like to deposit?');
-    }
-    else if (input === 'b') {
-    alert('Your account balance is $100.');
-    }
-    else if (input === 'q') {
-    alert('Your transaction is complete. Thank you for banking with us today!');
-    }
-    else {
-    alert('Invalid entry. Please try again.');
-    }
+userOption();
 
 }
